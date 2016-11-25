63135cb5d5dc8dfdec516bb46ef75c3a6bd3a147
Rebecca <rebeccamitchell88@gmail.com>
2016-11-25 14:28:09 -0500

    Initial Push

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Flickering");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Flickering() {
+        yield return new WaitForSeconds(0.1f);
+
+        Color tempColor = this.GetComponent<SpriteRenderer>().color;
+        float newAlpha = Random.Range(0.5f, 1.0f);
+        // Debug.Log("Flickering: " + newAlpha);
+        tempColor.a = newAlpha;
+        this.GetComponent<SpriteRenderer>().color = tempColor;
+
+        StartCoroutine("Flickering");
+    }
+}
