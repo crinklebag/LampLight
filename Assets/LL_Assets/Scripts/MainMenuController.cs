01cfca897d4ad43b273900631089282799488762
Rebecca <rebeccamitchell88@gmail.com>
2017-02-27 17:01:16 -0500

    - Scenes ---- added button functions to the top icons ---- added thicker lines to the top icons
    - MainMenuController
    ---- added back button functions
    ---- added a function that waits until the last line lerps then loads the scene

--- Assets/LL_Assets/Scripts/MainMenuController.cs
+++ Assets/LL_Assets/Scripts/MainMenuController.cs
@@ -7,6 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour, IDragHandler {
 
+    public Image[] brighterBars;
+    private Color32[] brighterBarsColors;
+    private float lerpColorTime = 0;
+
     public enum MenuState { Intro, SongSelect, BGSelect };
     [SerializeField] MenuState currentState;
     [SerializeField] MenuState lastState;
@@ -16,6 +20,7 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
     Vector3 menuVelocity = Vector3.zero;
 
     bool moveUI = false;
+    bool startGame = false;
     float destXPos;
     float smoothTime = 0.5f;
     float start = 0;
@@ -27,7 +32,21 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
         // Debug.Log("Hi");
         currentState = MenuState.Intro;
         lastState = MenuState.Intro;
-	}
+
+        brighterBarsColors = new Color32[brighterBars.Length];
+
+        for (int i = 0; i < brighterBarsColors.Length; i++)
+        {
+            brighterBarsColors[i] = brighterBars[i].color;
+        }
+
+        PlayerPrefs.SetInt("bgNumber", -1);
+        PlayerPrefs.SetInt("sceneNumber", -1);
+        PlayerPrefs.Save();
+
+        //Debug.Log(PlayerPrefs.GetInt("bgNumber"));
+        //Debug.Log(PlayerPrefs.GetInt("sceneNumber"));
+    }
 
     void Update() {
         MoveUI();
@@ -39,33 +58,59 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
         }
 
         ProcessInputs();*/
+
+        for (int i = 0; i < brighterBars.Length; i++)
+        {
+            brighterBars[i].color = Color32.Lerp(brighterBars[i].color, brighterBarsColors[i], lerpColorTime += Time.deltaTime);
+        }
+
+        if (startGame)
+        {
+            //Debug.Log("StartGame");
+            //Debug.Log(brighterBars[2].color.a);
+
+            if (brighterBars[2].color.a >= 1.0f)
+            {
+                LoadScene();
+                //Debug.Log("LoadScene");
+                startGame = false;
+            }
+        }
     }
 
     void UpdateMenuState(bool moveRight) {
         // Debug.Log("Hit Space");
-        switch (currentState) {
+
+        lerpColorTime = 0;
+
+        switch (currentState)
+        {
             case MenuState.Intro:
-                newPos = new Vector3(-(Screen.width * 1.19f), 0, 0);
+                newPos = new Vector3(0, 0, 0);
                 //lastState = MenuState.Intro;
                 //currentState = MenuState.SongSelect;
+                brighterBarsColors[0] = Color.clear;
+                brighterBarsColors[1] = Color.clear;
+                brighterBarsColors[2] = Color.clear;
                 break;
             case MenuState.SongSelect:
-                if (lastState == MenuState.BGSelect) {
-                    newPos = new Vector3(-(Screen.width * 2.38f), 0, 0);
-                    //lastState = MenuState.Intro;
-                    //currentState = MenuState.BGSelect;
-                } else {
-                    newPos = new Vector3(0, 0, 0);
-                    //lastState = MenuState.Intro;
-                    //currentState = MenuState.Intro;
-                }
+                newPos = new Vector3(-(Screen.width * 2.38f), 0, 0);
+                //lastState = MenuState.Intro;
+                //currentState = MenuState.BGSelect;
+                brighterBarsColors[0] = Color.white;
+                brighterBarsColors[1] = Color.white;
+                brighterBarsColors[2] = Color.clear;
                 break;
             case MenuState.BGSelect:
                 newPos = new Vector3(-(Screen.width * 1.19f), 0, 0);
                 //lastState = MenuState.BGSelect;
                 //currentState = MenuState.SongSelect;
+                brighterBarsColors[0] = Color.white;
+                brighterBarsColors[1] = Color.clear;
+                brighterBarsColors[2] = Color.clear;
                 break;
         }
+
         moveUI = true;
     }
 
@@ -145,20 +190,29 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
                 break;
             case 2:
                 {
-                    currentState = MenuState.SongSelect;
+                    int bgToSave = -1;
 
-                    int bgToSave = 0;
+                    if (EventSystem.current.currentSelectedGameObject.name == "Music")
+                    {
+                        currentState = MenuState.SongSelect;
+                    }
 
                     switch (EventSystem.current.currentSelectedGameObject.name)
                     {
                         case "Forest":
                             {
-                                bgToSave = 2;
+                                if (EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color.a == 0)
+                                {
+                                    bgToSave = 2;
+                                    currentState = MenuState.SongSelect;
+                                }
                             }
                             break;
                         default:
                             break;
                     }
+
+                    //Debug.Log(bgToSave);
 
                     PlayerPrefs.SetInt("bgNumber", bgToSave);
                     PlayerPrefs.Save();
@@ -168,7 +222,10 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
                 break;
             case 3:
                 {
-                    int sceneToSave = 0;
+                    lerpColorTime = 0;
+                    brighterBarsColors[2] = Color.white;
+
+                    int sceneToSave = -1;
 
                     switch (EventSystem.current.currentSelectedGameObject.name)
                     {
@@ -187,7 +244,7 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
                                 sceneToSave = 2;
                             }
                             break;
-                        case "Spirit Seeker":
+                        case "Spirit Speaker":
                             {
                                 sceneToSave = 3;
                             }
@@ -196,22 +253,46 @@ public class MainMenuController : MonoBehaviour, IDragHandler {
                             break;
                     }
 
+                    //Debug.Log(sceneToSave);
+
                     PlayerPrefs.SetInt("sceneNumber", sceneToSave);
                     PlayerPrefs.Save();
 
-                    // need to make scenes with the background and music in it already
-                    // for now just loads and finds the music/bg and sets it
-                    // since it is using ints, maybe have the scene named like Main_Mobile_01 for a backyard bg and Get Free music
-
-                    //string sceneName = "Main_Mobile_" + PlayerPrefs.GetInt("bgNumber").ToString() + PlayerPrefs.GetInt("sceneNumber").ToString();
-
-                    //SceneManager.LoadScene(sceneName);
-
-                    SceneManager.LoadScene("Main_Mobile");
+                    if (startGame == false)
+                    {
+                        startGame = true;
+                    }
                 }
                 break;
             default:
                 break;
+        }
+    }
+
+    void LoadScene()
+    {
+        //yield return new WaitUntil(() => brighterBars[2].color.a >= (brighterBarsColors[2].a - 1));
+
+        // need to make scenes with the background and music in it already
+        // for now just loads and finds the music/bg and sets it
+        // since it is using ints, maybe have the scene named like Main_Mobile_01 for a backyard bg and Get Free music
+
+        //string sceneName = "Main_Mobile_" + PlayerPrefs.GetInt("bgNumber").ToString() + PlayerPrefs.GetInt("sceneNumber").ToString();
+
+        //SceneManager.LoadScene(sceneName);
+
+        if (PlayerPrefs.GetInt("bgNumber") != -1 && PlayerPrefs.GetInt("sceneNumber") != -1)
+        {
+            //Debug.Log("Loading scene");
+            SceneManager.LoadScene("Main_Mobile");
+        } else
+        {
+            Debug.Log("You missed a selection!!");
+
+            Debug.Log("bgNumber " + PlayerPrefs.GetInt("bgNumber"));
+            Debug.Log("sceneNumber " + PlayerPrefs.GetInt("sceneNumber"));
+
+            startGame = false;
         }
     }
 }
