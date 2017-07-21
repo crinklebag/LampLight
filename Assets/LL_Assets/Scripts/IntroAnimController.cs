8f94ca84b17a5b4e2797d3b8e3998caa9d91d06b
Rebecca Mitchell <rebeccamitchell88@gmail.com>
2017-07-21 14:32:45 -0400

    - fixed a bug where the player could press the buttons on the main menu while the lamplight animation was still playing - readjusted the lamplight animation logo so it would line up with the logo on the main menu

--- Assets/LL_Assets/Scripts/IntroAnimController.cs
+++ Assets/LL_Assets/Scripts/IntroAnimController.cs
@@ -8,6 +8,9 @@ public class IntroAnimController : MonoBehaviour {
 	[SerializeField] float time;
 
 	public bool animIsDone = false;
+	public bool inMainMenu = false;
+
+	public GameObject[] thingsToHide;
 
 	void Awake()
 	{
@@ -16,8 +19,22 @@ public class IntroAnimController : MonoBehaviour {
 
 	IEnumerator StopAnim()
 	{
+		if (inMainMenu) {
+			// if in main menu (so if anyone touches where the buttons are, it doesn't trigger when this animation is still playing)
+			for (int i = 0; i < thingsToHide.Length; i++) {
+				thingsToHide [i].SetActive (false);
+			}
+		}
+
 		yield return new WaitForSeconds(time);
 		animIsDone = true;
+
+		if (inMainMenu) {
+			for (int i = 0; i < thingsToHide.Length; i++) {
+				thingsToHide [i].SetActive (true);
+			}
+		}
+
 		IntroAnim.SetActive(false);
 	}
 }
